dd23ec56e888c6ff1b58cba982fd38ebac7ae5d4
Dan Ross <daniellross@gmail.com>
2021-03-06 11:29:54 -0600

    added productType provider and set productList as its child. Implemented find method to identify the correct type and pass it on to the productcard

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,18 +1,26 @@
 import React, { useContext, useEffect } from "react"
 import { ProductContext } from "./ProductProvider"
+import { ProductTypeContext } from "./ProductTypeProvider"
 import { ProductCard } from "./Product"
 
 export const ProductList = () => {
     const { products, getProducts } = useContext(ProductContext)
+    const { productTypes, getProductTypes } = useContext(ProductTypeContext)
 
     useEffect(() => {
-        getProducts()
+        getProductTypes()
+            .then(getProducts)
     }, [])
 
     return (
-        <>
-            <h2> Check out our awesome products!</h2>
-            {products.map(product => <ProductCard key={product.id} product={product}/>)}
-        </>
+        <div>
+            <h2>Check out our awesome products!</h2>
+            {products.map(product => {
+
+                const type = productTypes.find(type => type.id === product.productTypeId )
+                return <ProductCard key={product.id} product={product} type={type}/>
+            })
+            }
+        </div>
     )
 }
--- src/components/products/ProductTypeProvider.js
+++ src/components/products/ProductTypeProvider.js
@@ -2,7 +2,7 @@ import React, { useState, createContext } from "react"
 
 export const ProductTypeContext = createContext()
 
-export const ProductProvider = props => {
+export const ProductTypeProvider = props => {
     const [productTypes, setProductTypes] = useState([])
 
     const getProductTypes = () => {
