b7ee8b228fc87ef881962c2e1e4da4db83f6713a
Dan Ross <daniellross@gmail.com>
2021-03-06 11:11:38 -0600

    refactored productProvider to not use expand. Created type provider.

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -6,7 +6,7 @@ export const ProductCard = ({ product }) => {
             <h3>Product: {product.name}</h3>
             <div>Price: ${product.price}</div>
             <div>Typeid: {product.productTypeId}</div>
-            <div>Type Name: {product.productType.name}</div>
+            {/* <div>Type Name: {product.productType.name}</div> */}
         </>
     )
 }
